2d8f62b9c488020d9eb07ae7708582a5beba1d90
Alamgir Hossain <gazialamgirhossainarzu@gmail.com>
2023-03-13 10:26:54 +0600

    Calculate Special Bonus

---
+++ 1873. Calculate Special Bonus/Calculate Special Bonus.sql
@@ -0,0 +1,9 @@
+SELECT  employee_id, CASE
+            WHEN MOD (employee_id, 2) <> 0 AND NAME NOT LIKE 'M%'
+            THEN
+               ( (salary * 100) / 100)
+            ELSE
+               0
+         END
+            bonus
+  FROM   employees;
