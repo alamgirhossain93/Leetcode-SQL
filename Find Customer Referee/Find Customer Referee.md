73d55a1432845308202352aa782d44801fc1572f
Alamgir Hossain <gazialamgirhossainarzu@gmail.com>
2023-09-20 11:20:09 +0600

    Find Customer Referee

---
+++ Find Customer Referee/Find Customer Referee.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/find-customer-referee/submissions/?envType=study-plan-v2&envId=top-sql-50
---
+++ Find Customer Referee/Find Customer Referee.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+select name
+from customer
+where referee_id != 2
+or referee_id is null
+order by name ;
