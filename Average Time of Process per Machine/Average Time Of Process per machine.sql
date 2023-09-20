80ad4adec940da8eb01556997e7f4a55462f8736
Alamgir Hossain <gazialamgirhossainarzu@gmail.com>
2023-09-20 11:45:01 +0600

    Average Time of Process per Machine

---
+++ Average Time of Process per Machine/Average Time Of Process per machine.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+SELECT a.machine_id, ROUND(avg(b.timestamp - a.timestamp), 3) as processing_time
+FROM Activity a, Activity b
+WHERE a.machine_id = b.machine_id 
+AND  a.process_id = b.process_id 
+AND a.activity_type = 'start' 
+AND b.activity_type = 'end'
+GROUP BY a.machine_id
+order by machine_id ;
