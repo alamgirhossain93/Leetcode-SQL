d565c976b5c014a2aca968fbf337accd6db59d7e
Alamgir Hossain <gazialamgirhossainarzu@gmail.com>
2023-09-23 02:01:57 +0600

    Queries Quality and Percentage

--- SQL 50/Queries Quality and Percentage/Queries Quality and Percentage.sql
+++ SQL 50/Queries Quality and Percentage/Queries Quality and Percentage.sql
@@ -1,17 +1,5 @@
-WITH QueryRatings AS (
-    SELECT
-        query_name,
-        AVG(rating / CAST(position AS NUMBER)) AS quality,
-        ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage
-    FROM
-        Queries
-    GROUP BY
-        query_name
-)
-
-SELECT
-    query_name,
-    ROUND(quality, 2) AS quality,
-    poor_query_percentage
-FROM
-    QueryRatings
+select  query_name , round(sum(rating/ position)/ count(query_name ),2) quality , 
+round (sum (case when rating <3 then 1 else 0 end )*100 / count(query_name ),2) poor_query_percentage 
+from Queries 
+group by query_name 
+order by query_name 
