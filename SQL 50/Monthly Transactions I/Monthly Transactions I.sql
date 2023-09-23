66aef41da57defcfc3bce6e07e536cea5b6362e2
Alamgir Hossain <gazialamgirhossainarzu@gmail.com>
2023-09-24 00:13:05 +0600

    Monthly Transactions I

---
+++ SQL 50/Monthly Transactions I/Monthly Transactions I.sql
@@ -0,0 +1,15 @@
+SELECT
+    TO_CHAR(trans_date, 'YYYY-MM')  month,
+    country,
+    COUNT(id)  trans_count ,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END)  approved_count ,
+    SUM(amount)  trans_total_amount ,
+    
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) approved_total_amount 
+FROM
+    Transactions
+GROUP BY   country, 
+    TO_CHAR(trans_date, 'YYYY-MM')
+   
+ORDER BY 
+    country desc , month;
